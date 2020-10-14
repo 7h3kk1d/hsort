05adb4a4d45f94ff88760a09f48b8b51301f3342
Alexander Bandukwala <7h3kk1d@gmail.com>
2020-10-13 20:48:53 -0500

    Use better prompts

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import           Data.Semigroup      ((<>))
 import           Lib
 import           Options.Applicative
+import System.IO (hFlush, stdout)
 
 params :: Parser String
 params = argument str (metavar "FILE")
@@ -13,13 +14,14 @@ fetchLines = (fmap . fmap) lines readFile
 -- TODO Give a better UX for making a decision.
 comparison :: String -> String -> IO Ordering
 comparison a b = do
-  putStrLn (a ++ " <> " ++ b)
+  putStr (a ++ " <> " ++ b ++ ": ")
+  hFlush stdout
   choice <- getLine
   case choice of
     "h" -> return GT
     "l" -> return LT
     _ -> do
-      putStrLn "Invalid Option"
+      putStrLn "Invalid Option. Please use h to pick the left option and l for the right."
       comparison a b
 
 main :: IO ()
